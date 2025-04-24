2ff8318126eaaeb5e8ccbd40db5dff658f6d641f
Adan <adanshahid.student@gmail.com>
2025-04-24 13:30:22 +0500

    Working on sign up and login page

--- Resources/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar.open');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
