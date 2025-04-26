2f729fabc355987fae91752fcb13bf884039612b
Adan <adanshahid.student@gmail.com>
2025-04-26 13:20:51 +0500

    Struggling to create a login form

--- mywebsite/web/urls.py
+++ mywebsite/web/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.loginPage, name="login"),
+    path('signup/', views.signupPage, name="signup"),
 ]
--- mywebsite/web/views.py
+++ mywebsite/web/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'navbar.html')
+    return render(request, 'main.html')
 
 def loginPage(request):
     return render(request, 'login.html')
+
+def signupPage(request):
+    return render(request,'signup.html')
